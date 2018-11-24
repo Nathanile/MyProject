135b01bbd82887f148e33f1036c9ae054b3aa150
apple <1322330238@qq.com>
2018-11-24 23:13:11 +0800

    新建个人工程

---
+++ MyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyProject
+//
+//  Created by Nathanile on 2018/11/24.
+//  Copyright © 2018年 Nathanile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyProject
+//
+//  Created by Nathanile on 2018/11/24.
+//  Copyright © 2018年 Nathanile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
